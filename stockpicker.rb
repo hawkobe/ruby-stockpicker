9d271fc42a66ba31543d0d763fffa263d2b4380a
Jacob Sanders <sanders.jms@gmail.com>
2022-06-13 01:37:33 -0600

    Wrote and finished code in one go! Could use some truncating

---
+++ stockpicker.rb
@@ -0,0 +1,19 @@
+def stock_picker(array)
+  differences = []
+
+  array.each_with_index do |price1, idx1|
+    array.each_with_index do |price2, idx2|
+      next if idx1 > idx2
+      if price2 > price1
+        differences << [(price2 - price1), idx2, idx1]
+      end
+    end
+  end
+
+  day_index = []
+
+  day_index.push(differences.max[2], differences.max[1])
+
+  day_index
+
+end
